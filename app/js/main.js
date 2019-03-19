f988cbf2086218fb5f9f900ac9aa790116b259c4
Serrjik <uac.sarge@gmail.com>
2019-03-19 23:29:21 +0300

    10-th commit - slider in header ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,31 @@
-console.log('Hello Surfing!');
+$(document).ready(function(){
+
+	const headerSlider = $('.owl-carousel');
+	
+	headerSlider.on('initialized.owl.carousel', function(event) {
+		$('.slide-controls-number__active').text(event.item.index + 1);
+		$('.slide-controls-number__total').text(event.item.count);
+	});
+
+	headerSlider.owlCarousel({
+		items: 1,
+		// loop: true,
+		dots: false,
+		smartSpeed: 2000
+	});
+
+	// Go to the previous item
+	$('#headerSliderLeft').click(function() {
+	    headerSlider.trigger('prev.owl.carousel');
+	});
+	// Go to the next item
+	$('#headerSliderRight').click(function() {
+	    headerSlider.trigger('next.owl.carousel');
+	});
+
+	headerSlider.on('changed.owl.carousel', function(event) {
+		$('.slide-controls-number__active').text(event.item.index + 1);
+		$('.slide-controls-number__total').text(event.item.count);
+	});
+
+});
