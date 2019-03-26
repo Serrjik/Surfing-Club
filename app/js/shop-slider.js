06b341279a40098062562989f1a877f67d605aff
Serrjik <uac.sarge@gmail.com>
2019-03-26 16:53:06 +0300

    15 commit - mobile adaptation in process -article block ready

--- app/js/shop-slider.js
+++ app/js/shop-slider.js
@@ -12,7 +12,17 @@ $(document).ready(function(){
 		loop: true,
 		dots: false,
 		smartSpeed: 500,
-		margin: 2
+		margin: 2,
+		responsive: {
+			// breakpoint from 0 up
+			0: {
+				items: 1,
+			},
+			// breakpoint from 1254 up
+			1254: {
+				items: 3,
+			}
+		}
 	});
 
 	// Go to the previous item
